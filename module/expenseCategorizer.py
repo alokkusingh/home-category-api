8157f5b55abbf1a6d22c77ef3c9da0f78c088d2f
alokkusingh <alok.ku.singh@gmail.com>
2023-12-28 19:11:10 +0530

    Updated model

--- module/expenseCategorizer.py
+++ module/expenseCategorizer.py
@@ -6,7 +6,7 @@ class ExpenseCategorizer:
 
     def __init__(self):
         print("ExpenseCategorizer initialized")
-        self.model = joblib.load("/Users/aloksingh/git/home-category-api/model/expense-categorization-mdl.joblib");
+        self.model = joblib.load("/Users/aloksingh/git/home-category-api/model/expense-categorization-LSVC-mdl.joblib");
         self.count_vect = joblib.load("/Users/aloksingh/git/home-category-api/model/expense-categorization-vct.joblib");
         print(self.model)
 
